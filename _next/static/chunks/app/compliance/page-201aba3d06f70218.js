6992fa6601dc9b4a641ea5d2e2e3fcb4bb62f64d
Sachin Tulla <14272138+sachintulla@users.noreply.github.com>
2026-08-11 10:46:26 +0530

    Publish new website-next static export to www.swiftloan.ai
    Replaces the old static marketing site with the statically-exported
    website-next (new UI). Adds .nojekyll so GitHub Pages serves _next/ assets;
    keeps CNAME and the brand-guide PDF.

---
+++ _next/static/chunks/app/compliance/page-201aba3d06f70218.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[780],{6618:function(n,e,t){Promise.resolve().then(t.t.bind(t,2972,23)),Promise.resolve().then(t.t.bind(t,4394,23))},4394:function(){}},function(n){n.O(0,[836,972,971,117,744],function(){return n(n.s=6618)}),_N_E=n.O()}]);
